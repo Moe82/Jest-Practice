0c0eb5f9522e0669395b49e4315aa5e044083cb5
Mohamed Shafee <mshafee1993@gmail.com>
2021-04-03 12:24:19 -0400

    add basic functionality of CommentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-	return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+	state = { comment: "" };
+
+	handleChange = (event) => {
+		const value = event.target.value;
+		this.setState({ [event.target.name]: value });
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.setState({ comment: "" });
+	};
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add a comment</h4>
+					<textarea
+						name={"comment"}
+						value={this.state.comment}
+						onChange={this.handleChange}
+					/>
+					<div>
+						<button>Submit Comment</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default CommentBox;
