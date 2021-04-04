58b45b7e0cebabff11f225c346f0901c97c3fe93
Mohamed Shafee <mshafee1993@gmail.com>
2021-04-04 10:09:25 -0400

    create comments reducer

---
+++ src/redux/reducers/comments.js
@@ -0,0 +1,12 @@
+import { SAVE_COMMENT } from 'redux/actions/types';
+
+const comments = (state = {}, action) => {
+	switch (action.type) {
+		default:
+			return state;
+		case SAVE_COMMENT:
+			return [...state, action];
+	}
+};
+
+export default comments;
