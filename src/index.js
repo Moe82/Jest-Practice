b2d8efb1c5afb1c863e96c0eecd0eebb48c8c102
Mohamed Shafee <mshafee1993@gmail.com>
2021-04-04 11:17:43 -0400

    leverage functionality of Root.js to create root component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
-import store from 'redux/store';
 import App from 'components/App';
+import Root from 'Root';
 
 ReactDOM.render(
-	<Provider store={store}>
+	<Root>
 		<App />
-	</Provider>,
+	</Root>,
 	document.querySelector('#root')
 );
